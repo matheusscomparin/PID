3e95c9f18111d525d43718cece05846f07d77b9f
Ubuntu <ubuntu@ip-172-31-59-133.ec2.internal>
2020-11-17 13:17:54 +0000

    att

--- teste1.py
+++ teste1.py
@@ -188,94 +188,95 @@ def timerCallBack(event):
         control = 0
         msg.angular.z = 0
     
-    if estado == 1:
-        if min(scan.ranges[scan_len-10 : scan_len+10]) < 100:
-            print('entrou no controle')
-            msg.angular.z = 0
-            yaw = getAngle(odom)
-            dir_obj = min(scan.ranges[scan_len-10 : scan_len+10])
-            
-            setpoint = (dir_obj - scan.ranges[0])/(scan.ranges[scan_len-1] - scan.ranges[0]) #interpolacao
-            setpoint *= 200 #interpolacao
-            setpoint -= 100 #interpolacao
-        
-            ind = scan.ranges.index(min(scan.ranges))
-            inc = 2*math.pi / scan_len
-            ang = (ind * inc * 180.0/math.pi) + yaw
-            if ang > 180:
-                ang -= 360
+    else:
+        if estado == 1:
+            if min(scan.ranges[scan_len-10 : scan_len+10]) < 100:
+                print('entrou no controle')
+                msg.angular.z = 0
+                yaw = getAngle(odom)
+                dir_obj = min(scan.ranges[scan_len-10 : scan_len+10])
                 
-            error = (ang - yaw)
+                setpoint = (dir_obj - scan.ranges[0])/(scan.ranges[scan_len-1] - scan.ranges[0]) #interpolacao
+                setpoint *= 200 #interpolacao
+                setpoint -= 100 #interpolacao
             
-            if abs(error) > 180:
-                if setpoint < 0:
-                    error += 360 
-                else:
-                    error -= 360
+                ind = scan.ranges.index(min(scan.ranges))
+                inc = 2*math.pi / scan_len
+                ang = (ind * inc * 180.0/math.pi) + yaw
+                if ang > 180:
+                    ang -= 360
                     
-            #print(ang, yaw, error)
+                error = (ang - yaw)
+                
+                if abs(error) > 180:
+                    if setpoint < 0:
+                        error += 360 
+                    else:
+                        error -= 360
+                        
+                #print(ang, yaw, error)
+                
+                delta_e = error - old_error
+                old_error = error
+                 
+                P = kp*error
+                Int += error*tempo_loop
+                I = Int * ki
+                D = delta_e * kd
+                
+                control = P+I+D
+                if control > 1:
+                    control = 1
+                elif control < -1:
+                    control = -1
+                
+            else:
+                #if min(scan.ranges[scan_len-15 : scan_len+15]) < 100: #se nao enncontrou o objeto roda ate achar
+                   # msg.angular.z = 0.15
+                #else:
+                control = 0.3
+                print('rodando')
             
-            delta_e = error - old_error
-            old_error = error
-             
-            P = kp*error
-            Int += error*tempo_loop
-            I = Int * ki
-            D = delta_e * kd
             
-            control = P+I+D
-            if control > 1:
-                control = 1
-            elif control < -1:
-                control = -1
+            msg.angular.z = control
+            pub.publish(msg)
             
-        else:
-            #if min(scan.ranges[scan_len-15 : scan_len+15]) < 100: #se nao enncontrou o objeto roda ate achar
-               # msg.angular.z = 0.15
-            #else:
-            control = 0.3
-            print('rodando')
+            if abs(error) < 1:
+                Int = 0
+                estado = 2
+                print('SEGUNDO ESTADO')
+                
         
-        
-        msg.angular.z = control
-        pub.publish(msg)
-        
-        if abs(error) < 1:
-            Int = 0
-            estado = 2
-            print('SEGUNDO ESTADO')
+        if estado == 2:
+            print('COMECA ANDAR')
+            read = min(scan.ranges)
+            setpoint = 0.5 # 50 cm do objeto
+            msg.angular.z = 0
+            scan_len = len(scan.ranges)
+            if scan_len > 0:
+                real_dist = min(scan.ranges[scan_len-10 : scan_len+10])
+                
+                error = -(setpoint - real_dist)
+                delta_e = error - old_error
+                old_error = error
+                 
+                P = kp*error
+                Int += error*tempo_loop
+                I = Int * ki
+                D = delta_e * kd
+                
+                control = P+I+D
+                if control > 1:
+                    control = 1
+                elif control < -1:
+                    control = -1
+            else:
+                control = 0        
             
-    
-    if estado == 2:
-        print('COMECA ANDAR')
-        read = min(scan.ranges)
-        setpoint = 0.5 # 50 cm do objeto
-        msg.angular.z = 0
-        scan_len = len(scan.ranges)
-        if scan_len > 0:
-            real_dist = min(scan.ranges[scan_len-10 : scan_len+10])
             
-            error = -(setpoint - real_dist)
-            delta_e = error - old_error
-            old_error = error
-             
-            P = kp*error
-            Int += error*tempo_loop
-            I = Int * ki
-            D = delta_e * kd
-            
-            control = P+I+D
-            if control > 1:
-                control = 1
-            elif control < -1:
-                control = -1
-        else:
-            control = 0        
-        
-        
-        msg = Twist()
-        msg.linear.x = control
-        pub.publish(msg)
+            msg = Twist()
+            msg.linear.x = control
+            pub.publish(msg)
         
 
 pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
